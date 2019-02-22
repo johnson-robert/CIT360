f6becb4143f9424542359567c4eebcd6a3181190
rob johnson <robajohnson72@gmail.com>
2019-02-22 08:03:05 -0800

    initial commit

--- src/acmeConnection/Controller.java
+++ src/acmeConnection/Controller.java
@@ -5,8 +5,13 @@
  */
 package acmeConnection;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -14,23 +19,46 @@ import org.hibernate.SessionFactory;
  *
  * @author User
  */
-public class Controller {
 
-    private static final SessionFactory sessionFactory;
-    
-    static {
+public class Controller {
+    private static SessionFactory sessionFactory;
+    //use static if using view
+    public static void main(String[] args){
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
+            System.out.println("Made it this far.");
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (HibernateException ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+    //for using view
+//    public static SessionFactory getSessionFactory() {
+//        return sessionFactory;
+//    }
+
+ // Method to  READ all the employees
+   public void listCategories( ){
+      Session session = sessionFactory.openSession();
+      Transaction tx = null;
+      
+      try {
+         System.out.println("Made it this far too.");
+         tx = session.beginTransaction();
+         List category = session.createQuery("FROM Categories").list(); 
+         for (Iterator iterator = category.iterator(); iterator.hasNext();){
+            Categories categories = (Categories) iterator.next(); 
+            System.out.print("First Name: " + categories.getCategoryId()); 
+            //System.out.print("  Last Name: " + employee.getLastName()); 
+            //System.out.println("  Salary: " + employee.getSalary()); 
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         System.out.println("Made it too here."); 
+         if (tx!=null) tx.rollback();
+         e.printStackTrace(); 
+      } finally {
+         session.close(); 
+      }
+   }
 }
